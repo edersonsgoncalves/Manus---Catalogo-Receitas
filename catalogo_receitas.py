8faf813ab02065bc0c050aa13fb8fc3030490d38
ederson <edersonsgoncalves@gmail.com>
2025-07-13 20:25:04 -0300

    feat: criar classe Receita

---
+++ catalogo_receitas.py
@@ -0,0 +1,6 @@
+class Receita:
+    def __init__(self, nome:str, ingredientes:list, modo_preparo:str):
+        self.nome = nome
+        self.ingredientes = ingredientes
+        self.modo_preparo = modo_preparo
+        
